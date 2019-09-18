a487804a2972d455a7e3f96ad1b105361a8ea1b1
masito16 <54905796+masito16@users.noreply.github.com>
2019-09-18 16:54:31 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,24 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+    var numero;
+	var promedio;
+
+	for ( var i=0; i <5; i=i+1) { 
+
+		numero = parseInt( prompt("Ingrese un numero: "));
+
+		acumulador = acumulador + numero;
 
 
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
 
+
+
+
 }//FIN DE LA FUNCIÓN
+
+i=i+1
