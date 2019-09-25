a11f91d4833a53855e656a952d7c7102b5e0e817
masito16 <54905796+masito16@users.noreply.github.com>
2019-09-25 16:55:30 -0300

    Add files via upload

---
+++ Curso JSPAMI/Calcular radio/js/script.js
@@ -0,0 +1,12 @@
+//let cadena= "Ferrocarril Oeste de CAballito";
+//console.log(cadena.startsWith('Fr') );*//..
+
+let perro = {
+    nombre : "Bobby",
+    edad : 3, 
+    patas : 4, 
+    raza : "caniche",
+    
+}
+
+console.log("Antes de la funcion el perro se llama " + perro.nombre);
